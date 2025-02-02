5ee804a96836bdd548fc052de523c0ed8ac95ed0
Nicola31082005 <nikule512@gmail.com>
2025-02-02 13:56:33 +0200

    add front-end javascript logic

---
+++ public/src/app.js
@@ -0,0 +1,22 @@
+const groupSelect = document.querySelector('#groupSelect');
+const rowsNodeList = document.querySelectorAll('.mainTable tbody tr:not(.mainRow)')
+
+groupSelect.addEventListener('change', (e) => {
+    filterTableData(e)
+});
+
+
+function filterTableData(e) {
+    const currentGroup = e.target.value;
+
+    rowsNodeList.forEach(row => {
+        const group = row.getAttribute('data-group');
+
+        if (!currentGroup || group === currentGroup) {
+            row.style.display = 'table-row';
+        } else {
+            row.style.display = 'none';
+        }
+    });
+}
+
