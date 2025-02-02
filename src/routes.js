94ab9a9054f4197bbce679fab454da53fb0ef7a4
Nicola31082005 <nikule512@gmail.com>
2025-02-02 14:25:52 +0200

    make home view and add css

--- src/routes.js
+++ src/routes.js
@@ -5,9 +5,9 @@ import programService from "../services/programService.js";
 const routes = Router();
 const url = 'https://www.unwe.bg/bg/timetables/semesterresult?degID=1&cityID=1&formID=1&spec=2380&sem=2';
 
-// routes.get('/', (req, res) => {
-//     res.render('table', { layout: false });
-// });
+routes.get('/', (req, res) => {
+    res.render('home', { layout: false });
+});
 
 routes.get('/render-table', async (req, res) => {
     // Launch the browser and open a new blank page
